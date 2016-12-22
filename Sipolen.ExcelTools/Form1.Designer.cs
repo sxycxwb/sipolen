4b0bdd1aad497b131fcc1a6b712fe5be3d6cd290
许王斌 <xuwangbin@sinldo.com>
2016-12-22 18:39:20 +0800

    提交模板信息，修改样式页面

--- Sipolen.ExcelTools/Form1.Designer.cs
+++ Sipolen.ExcelTools/Form1.Designer.cs
@@ -28,34 +28,221 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnTest = new System.Windows.Forms.Button();
+            this.btnSelectSourceExcel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbCountry = new System.Windows.Forms.ComboBox();
+            this.cbCountryTemplate = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtWorkPath = new System.Windows.Forms.TextBox();
+            this.btnSetWorkPath = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.rBtnSelectSite = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.lbCurrencyInfo = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // btnTest
+            // btnSelectSourceExcel
             // 
-            this.btnTest.Location = new System.Drawing.Point(48, 64);
-            this.btnTest.Name = "btnTest";
-            this.btnTest.Size = new System.Drawing.Size(75, 23);
-            this.btnTest.TabIndex = 0;
-            this.btnTest.Text = "测试";
-            this.btnTest.UseVisualStyleBackColor = true;
-            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            this.btnSelectSourceExcel.Location = new System.Drawing.Point(33, 55);
+            this.btnSelectSourceExcel.Name = "btnSelectSourceExcel";
+            this.btnSelectSourceExcel.Size = new System.Drawing.Size(99, 23);
+            this.btnSelectSourceExcel.TabIndex = 0;
+            this.btnSelectSourceExcel.Text = "选择平台导出表";
+            this.btnSelectSourceExcel.UseVisualStyleBackColor = true;
+            this.btnSelectSourceExcel.Click += new System.EventHandler(this.btnSelectSourceExcel_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.LimeGreen;
+            this.label1.Location = new System.Drawing.Point(164, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Tag = "";
+            this.label1.Text = "选择文件信息";
+            // 
+            // cbCountry
+            // 
+            this.cbCountry.FormattingEnabled = true;
+            this.cbCountry.Location = new System.Drawing.Point(291, 102);
+            this.cbCountry.Name = "cbCountry";
+            this.cbCountry.Size = new System.Drawing.Size(80, 20);
+            this.cbCountry.TabIndex = 2;
+            // 
+            // cbCountryTemplate
+            // 
+            this.cbCountryTemplate.FormattingEnabled = true;
+            this.cbCountryTemplate.Location = new System.Drawing.Point(443, 102);
+            this.cbCountryTemplate.Name = "cbCountryTemplate";
+            this.cbCountryTemplate.Size = new System.Drawing.Size(121, 20);
+            this.cbCountryTemplate.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label2.Location = new System.Drawing.Point(228, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Tag = "";
+            this.label2.Text = "选择国家";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label3.Location = new System.Drawing.Point(383, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Tag = "";
+            this.label3.Text = "选择模板";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(33, 271);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "开始移表";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label4.Location = new System.Drawing.Point(31, 20);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(101, 12);
+            this.label4.TabIndex = 7;
+            this.label4.Tag = "";
+            this.label4.Text = "我的工作目录 -->";
+            // 
+            // txtWorkPath
+            // 
+            this.txtWorkPath.BackColor = System.Drawing.SystemColors.Control;
+            this.txtWorkPath.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.txtWorkPath.Location = new System.Drawing.Point(166, 17);
+            this.txtWorkPath.Name = "txtWorkPath";
+            this.txtWorkPath.Size = new System.Drawing.Size(320, 21);
+            this.txtWorkPath.TabIndex = 8;
+            this.txtWorkPath.Text = "D:\\work\\excel";
+            // 
+            // btnSetWorkPath
+            // 
+            this.btnSetWorkPath.Location = new System.Drawing.Point(595, 17);
+            this.btnSetWorkPath.Name = "btnSetWorkPath";
+            this.btnSetWorkPath.Size = new System.Drawing.Size(75, 23);
+            this.btnSetWorkPath.TabIndex = 9;
+            this.btnSetWorkPath.Text = "设置工作目录";
+            this.btnSetWorkPath.UseVisualStyleBackColor = true;
+            this.btnSetWorkPath.Click += new System.EventHandler(this.btnSetWorkPath_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label5.Location = new System.Drawing.Point(31, 105);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 12);
+            this.label5.TabIndex = 10;
+            this.label5.Tag = "";
+            this.label5.Text = "选择站点";
+            // 
+            // rBtnSelectSite
+            // 
+            this.rBtnSelectSite.AutoSize = true;
+            this.rBtnSelectSite.Checked = true;
+            this.rBtnSelectSite.Location = new System.Drawing.Point(90, 104);
+            this.rBtnSelectSite.Name = "rBtnSelectSite";
+            this.rBtnSelectSite.Size = new System.Drawing.Size(59, 16);
+            this.rBtnSelectSite.TabIndex = 11;
+            this.rBtnSelectSite.TabStop = true;
+            this.rBtnSelectSite.Text = "欧洲站";
+            this.rBtnSelectSite.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(155, 104);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(59, 16);
+            this.radioButton1.TabIndex = 12;
+            this.radioButton1.Text = "美洲站";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // lbCurrencyInfo
+            // 
+            this.lbCurrencyInfo.AutoSize = true;
+            this.lbCurrencyInfo.ForeColor = System.Drawing.Color.Blue;
+            this.lbCurrencyInfo.Location = new System.Drawing.Point(576, 79);
+            this.lbCurrencyInfo.Name = "lbCurrencyInfo";
+            this.lbCurrencyInfo.Size = new System.Drawing.Size(65, 12);
+            this.lbCurrencyInfo.TabIndex = 13;
+            this.lbCurrencyInfo.Tag = "";
+            this.lbCurrencyInfo.Text = "货币单位：";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.Blue;
+            this.label6.Location = new System.Drawing.Point(576, 106);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 12);
+            this.label6.TabIndex = 14;
+            this.label6.Tag = "";
+            this.label6.Text = "货币汇率：";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(692, 476);
-            this.Controls.Add(this.btnTest);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lbCurrencyInfo);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.rBtnSelectSite);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnSetWorkPath);
+            this.Controls.Add(this.txtWorkPath);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbCountryTemplate);
+            this.Controls.Add(this.cbCountry);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSelectSourceExcel);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.Button btnSelectSourceExcel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbCountry;
+        private System.Windows.Forms.ComboBox cbCountryTemplate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtWorkPath;
+        private System.Windows.Forms.Button btnSetWorkPath;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RadioButton rBtnSelectSite;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label lbCurrencyInfo;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- Sipolen.ExcelTools/Form1.cs
+++ Sipolen.ExcelTools/Form1.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Sipolen.Code;
 
 namespace Sipolen.ExcelTools
 {
@@ -19,7 +22,55 @@ namespace Sipolen.ExcelTools
 
         private void btnTest_Click(object sender, EventArgs e)
         {
+            var table = ExcelHelper.RenderDataTableFromExcel("test.xls", 0, 2);
+        }
 
+        private void btnSelectSourceExcel_Click(object sender, EventArgs e)
+        {
+            //初始化一个OpenFileDialog类
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "(*.xls)|*.xls|(*.xlsx)|*.xlsx";
+            //判断用户是否正确的选择了文件
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                //获取用户选择文件的后缀名
+                string extension = Path.GetExtension(fileDialog.FileName);
+                //声明允许的后缀名
+                string[] str = new string[] { ".xls", ".xlsx" };
+                if (!((IList)str).Contains(extension))
+                {
+                    MessageBox.Show("仅能选择xls,xlsx类型的Excel导出表！");
+                }
+                else
+                {
+                    //获取用户选择的文件
+                    FileInfo fileInfo = new FileInfo(fileDialog.FileName);
+                    //if (fileInfo.Length > 20480)
+                    //{
+                    //    MessageBox.Show("上传的图片不能大于20K");
+                    //}
+                    //else
+                    //{
+                    //    //在这里就可以写获取到正确文件后的代码了
+                    //}
+                }
+            }
+        }
+
+        /// <summary>
+        /// 设置工作目录
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSetWorkPath_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "请设置工作目录";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                string foldPath = dialog.SelectedPath;
+                txtWorkPath.Text = foldPath;
+            }
         }
     }
 }
